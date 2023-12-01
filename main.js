b8eac842fc19ba5baffc6657ebeaade170461655
SUBHANQURESHI0 <141053308+SUBHANQURESHI0@users.noreply.github.com>
2023-12-01 14:25:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+import inquirer from 'inquirer';
+class Student {
+    constructor(firstName, lastName, studentID, courses, balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentID = studentID;
+        this.courses = courses;
+        this.balance = balance;
+    }
+    enroll(courseName) {
+        this.courses.push(courseName);
+        console.log(`${courseName} has been added to the courses enrolled by ${this.firstName} ${this.lastName}.`);
+    }
+    viewBalance() {
+        console.log(`The balance of ${this.firstName} ${this.lastName} is $${this.balance}.`);
+    }
+    payTuition(amount) {
+        this.balance -= amount;
+        console.log(`${amount} has been paid by ${this.firstName} ${this.lastName}.`);
+    }
+    showStatus() {
+        console.log(`Name: ${this.firstName} ${this.lastName}`);
+        console.log(`Student ID: ${this.studentID}`);
+        console.log(`Courses Enrolled: ${this.courses}`);
+        console.log(`Balance: $${this.balance}`);
+    }
+}
+function generateStudentID() {
+    return Math.floor(Math.random() * 90000) + 10000;
+}
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'firstName',
+        message: "Enter first name:"
+    },
+    {
+        type: 'input',
+        name: 'lastName',
+        message: "Enter last name:"
+    },
+    {
+        type: 'number',
+        name: 'courseCount',
+        message: "How many courses do you want to enroll in?"
+    }
+]).then(answers => {
+    let studentID = generateStudentID();
+    let courses = [];
+    let balance = 0;
+    let student1 = new Student(answers.firstName, answers.lastName, studentID, courses, balance);
+    for (let i = 0; i < answers.courseCount; i++) {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'courseName',
+                message: `Enter course ${i + 1} name:`
+            }
+        ]).then(answers => {
+            student1.enroll(answers.courseName);
+        });
+    }
+    inquirer.prompt([
+        {
+            type: 'number',
+            name: 'paymentAmount',
+            message: "Enter payment amount:"
+        }
+    ]).then(answers => {
+        student1.payTuition(answers.paymentAmount);
+        student1.showStatus();
+    });
+});
